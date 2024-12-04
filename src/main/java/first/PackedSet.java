fd2ad18aecc16bd942cf0987d45d4aea79971d53
krefature <77618655+krefature@users.noreply.github.com>
2024-12-04 09:16:39 +0600

    refactoring PackedSet

--- src/main/java/first/PackedSet.java
+++ src/main/java/first/PackedSet.java
@@ -3,42 +3,36 @@ package first;
 public class PackedSet {
     private final Pack pack;
     private final Object[] objects;
-    public PackedSet(Object[] objects, Pack pack){
+
+    public PackedSet(Object[] objects, Pack pack) {
         this.objects = objects;
         this.pack = pack;
     }
-    public double getNetWeight(){
+
+    public double getNetWeight() {
         double netWeight = 0;
         for (Object obj : objects) {
-            if (obj.getClass() == PackedSet.class)
-            {
-                netWeight += ( (PackedSet) obj).getGrossWeight();
-            }
-            if (obj.getClass() == PackedPieceProduct.class)
-            {
-                netWeight += ( (PackedPieceProduct) obj).getGrossWeight();
-            }
-            if (obj.getClass() == PackedWeightProduct.class)
-            {
-                netWeight += ( (PackedWeightProduct) obj).getGrossWeight();
+            if (obj.getClass() == PackedSet.class) {
+                netWeight += ((PackedSet) obj).getGrossWeight();
+            } else if (obj.getClass() == PackedPieceProduct.class) {
+                netWeight += ((PackedPieceProduct) obj).getGrossWeight();
+            } else if (obj.getClass() == PackedWeightProduct.class) {
+                netWeight += ((PackedWeightProduct) obj).getGrossWeight();
             }
         }
         return netWeight;
     }
 
-    public double getGrossWeight(){
+    public double getGrossWeight() {
         double netWeight = pack.getWeight();
         for (Object obj : objects) {
-            if (obj.getClass() == PackedSet.class)
-            {
+            if (obj.getClass() == PackedSet.class) {
                 netWeight += ((PackedSet) obj).getGrossWeight();
             }
-            if (obj.getClass() == PackedPieceProduct.class)
-            {
+            if (obj.getClass() == PackedPieceProduct.class) {
                 netWeight += ((PackedPieceProduct) obj).getGrossWeight();
             }
-            if (obj.getClass() == PackedWeightProduct.class)
-            {
+            if (obj.getClass() == PackedWeightProduct.class) {
                 netWeight += ((PackedWeightProduct) obj).getGrossWeight();
             }
         }
